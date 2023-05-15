64de66e4905652f4f0fcc739bc5981c5b0d623fc
Lucas Barbosa <lucasbarbosaemail@gmail.com>
2023-05-15 10:59:23 +0100

    camera and map added

--- main.js
+++ main.js
@@ -3,8 +3,101 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const Menu = electron.Menu;
 
 let mainWindow = null;
+
+const menuItems = [
+    {
+        label: "File",
+        submenu: [
+
+            {
+                label: "Map",
+                click: async () => {
+                    const win2 = new BrowserWindow({
+                    height: 300,
+                    width: 400
+                    });
+
+                    win2.loadURL("https://www.google.com/maps/@53.3450488,-6.2680012,14z");
+                },
+            },
+            {
+                type: "separator",
+            },
+            {
+                label: "Help"
+            },
+            {
+                type: "separator",
+            },
+            {
+                label: "Learn more"
+            },
+            {
+                label: "Exit",
+                click: () => app.quit()
+            }
+        ]
+    },
+
+    {
+        label: 'View',
+        submenu: [
+          { role: 'reload' },
+          { type: 'separator' },
+          { role: 'resetZoom' },
+          { role: 'zoomIn' },
+          { role: 'zoomOut' },
+          { type: 'separator' },
+          { role: 'togglefullscreen' }
+        ]
+      },
+
+    {
+        label: "MoveApp",
+        submenu: [
+            {
+                label: "About moveApp"
+            },
+            { type: 'separator' },
+            { role: 'services' },
+        ]
+    },
+
+    {
+        label: "Account",
+        submenu: [
+            {
+                label: "User Account"
+            },
+            {
+                label: "Open camera",
+                click: async () => {
+                    const win3 = new BrowserWindow({
+                    height: 750,
+                    width: 650
+                    });
+                    
+                    win3.loadFile("camera.html");
+                    win3.once("ready-to-show"), ()=> win3.show();
+                },
+            },
+
+            {
+                type: "separator",
+            },
+            {
+                label: "Log out"
+            }
+        ]
+    },
+
+];
+
+const menu = Menu.buildFromTemplate(menuItems);
+Menu.setApplicationMenu(menu);
 
 app.on('window-all-closed', () => {
     if (Process.platform !== 'darwin') app.quit();
